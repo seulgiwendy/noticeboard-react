d6accbed7766a641444304797931ce887d220ab0
hwijun.jeong <hwijun.jeong@navercorp.com>
2018-08-06 17:34:13 +0900

    [shopping/Issue#1300] 헤더 컴포넌트 분리, 의존성 추가

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './header.css';
+import { Button, Modal } from 'react-materialize';
 
 const Header = (props) => {
     return(
         <div className="main-header">
             <img className="header-img" src="https://ssl.pstatic.net/imgshopping/adcenter/web/ico-shopping.png" width="117" height="22"/>
+            <Modal id="new-article" header="새 글 등록" trigger={<Button className="header-newarticle">공지 등록</Button>}>
+                <iframe className="new-article-editorbox" src="https://m.naver.com" />
+            </Modal>
         </div>
+
     )
 }
 
